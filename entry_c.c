79ace3c3bcc93dd63e0c9767ddbaed5a1e7fe6ea
CYBcaoyibo <cybcaoyibo@126.com>
2014-11-09 21:56:11 +0800

    test tile

--- entry_c.c
+++ entry_c.c
@@ -1,40 +1,41 @@
 #include <stdint.h>
 
-//240*160
+//lcd size = 240*160
+//buffer size = 256*256
 
-const char test_data[] =
-	"                        "
-	"    ##  #   #  ####     "
-	"   #    #   #  #   #    "
-	"  #     #   #  #   #    "
-	"  #     #   #  #   #    "
-	"  #      # #   #   #    "
-	"  #       #    #  #     "
-	"  #       #    ###      "
-	"  #       #    #  #     "
-	"  #       #    #   #    "
-	"  #       #    #   #    "
-	"  #       #    #   #    "
-	"  #       #    #   #    "
-	"   #      #    #   #    "
-	"    ##    #    ####     "
-	"                        ";
-
-void draw_test() {
-	uint8_t *vram = (uint8_t*)0x06000000;
-	uint8_t x, y;
-	for(y = 0; y < 160; y++) {
-		for(x = 0; x < 240; x++) {
-			if(test_data[y / 10 * 24 + x / 10] != ' ')
-				*vram = 2;
-			else
-				*vram = ((x % 20) < 10) ^ ((y % 20) < 10);
-			vram++;
+void init_lcd() {
+	//mode 0, bg 3
+	*(uint16_t*)0x04000000 = 0x0800;
+	//bg3 setting
+	*(uint16_t*)0x0400000E = 0x0880;
+	//test tile
+	uint16_t i, j, ind = 0;
+	for(i = 0; i < 8; i++) {
+		((uint32_t*)0x06000000)[i * 2 + 0] = 0x03020100;
+		((uint32_t*)0x06000000)[i * 2 + 1] = 0x07060504;
+	}
+	ind = 0;
+	for(i = 0; i < 8; i++) {
+		((uint32_t*)0x06000040)[i * 2 + 0] = 0x08080808;
+		((uint32_t*)0x06000040)[i * 2 + 1] = 0x08080808;
+	}
+	//test palette
+	for(i = 0; i < 8; i++) {
+		uint8_t tmp = i * 31 / 8;
+		((uint16_t*)0x05000000)[i] = 0x001F | (tmp << 5) | (tmp << 10);
+	}
+	((uint16_t*)0x05000000)[8] = 0x7C00;
+	//test map
+	ind = 0;
+	for(i = 0; i < 32; i++) {
+		for(j = 0; j < 32; j++) {
+			((uint16_t*)0x06004000)[ind] = (i % 2) ^ (j % 2);
+			ind++;
 		}
 	}
 }
 
 void entry_c() {
-	draw_test();
+	init_lcd();
 	while(1);
 }
