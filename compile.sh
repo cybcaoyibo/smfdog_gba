6d0505c0d3f30b2cda182ec468bf7b308a85b897
CYBcaoyibo <cybcaoyibo@126.com>
2014-11-11 15:50:37 +0800

    use thumb libc

--- compile.sh
+++ compile.sh
@@ -6,5 +6,5 @@ g++ -o imgconv imgconv.cpp -std=c++0x
 arm-none-eabi-gcc -c -o smfdog.o smfdog.c
 arm-none-eabi-as -o entry.o entry.S
 arm-none-eabi-gcc -c -o entry_c.o -O3 -mthumb -mcpu=arm7tdmi -msoft-float entry_c.c
-arm-none-eabi-ld --gc-sections -s -o out.elf entry.o entry_c.o smfdog.o /usr/local/arm-none-eabi/lib/thumb/libm.a /usr/local/arm-none-eabi/lib/libc.a /usr/local/lib/gcc/arm-none-eabi/4.9.1/thumb/libgcc.a -T ld.ld
+arm-none-eabi-ld --gc-sections -s -o out.elf entry.o entry_c.o smfdog.o /usr/local/arm-none-eabi/lib/thumb/libm.a /usr/local/arm-none-eabi/lib/thumb/libc.a /usr/local/lib/gcc/arm-none-eabi/4.9.1/thumb/libgcc.a -T ld.ld
 arm-none-eabi-objcopy -O binary out.elf out.gba
